b7d43d64a6f8d35f497e88b2de8f94e03a9b7e1c
ChunRen Zhang <rayatn1011@gmail.com>
2022-11-01 23:43:18 +0800

    feat: add router and login page

---
+++ src/features/router/index.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Login from '@/pages/login'
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Login />,
+    },
+])
