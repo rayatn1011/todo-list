a44001cab7086110b362d39874587e2bff712f6b
ChunRen Zhang <rayatn1011@gmail.com>
2022-11-05 22:35:01 +0800

    style: add blank line to gap import and export

--- src/features/i18n/index.ts
+++ src/features/i18n/index.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { isDev } from '@/features/utils'
+
 export default i18n
     .use(Backend)
     .use(LanguageDetector)
