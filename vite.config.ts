56cefa9c627dd17c24e76e8d7fbebdc73d733c3d
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-08 21:36:06 +0800

    chore: configure env

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [react(), eslint()],
+    envDir: './env',
     resolve: {
         alias: {
             '@': resolve(__dirname, 'src'),
         },
     },
-    // dev
+    /**
+     * dev config
+     */
     server: {
         open: true,
     },
-})
+    /**
+     * build config
+     */
+});
