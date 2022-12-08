2653d9c1134be8a05e0685fa753a55046b1efa2b
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-08 22:53:59 +0800

    feat: change i18n init time

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import '@/index.css'
 import App from '@/App'
+import '@/features/i18n'; // init i18n
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     // <React.StrictMode>
