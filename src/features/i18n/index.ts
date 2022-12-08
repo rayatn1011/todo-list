a137284ea5068d2cd0192b124281a64016ad9f22
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-08 22:55:27 +0800

    style: eslint fix

--- src/features/i18n/index.ts
+++ src/features/i18n/index.ts
@@ -1,7 +1,7 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import Backend from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 export default i18n
     .use(Backend)
@@ -17,7 +17,7 @@ export default i18n
         },
         fallbackLng: 'en',
         supportedLngs: ['en', 'zh-TW', 'zh', 'ja'],
-    })
+    });
 
 /**
  * mapping locale to html lang
