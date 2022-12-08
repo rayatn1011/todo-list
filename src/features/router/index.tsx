11db2fa75bbf9fef4383f735e3976ed734ceda12
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-08 23:26:58 +0800

    feat: configure router

--- src/features/router/index.tsx
+++ src/features/router/index.tsx
@@ -1,9 +1,52 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Login from '@/pages/login'
+import { lazy, Suspense } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import ProtectedRoute from '@/features/router/protectedRoute';
+
+/**
+ * Page Component
+ */
+import NotFound from '@/pages/notFound';
+/**
+ * lazy Page Component
+ */
+const HomePage = lazy(() => import('@/pages/home'));
+const SignInPage = lazy(() => import('@/pages/signIn'));
+const SignUpPage = lazy(() => import('@/pages/signUp'));
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Login />,
+        element: (
+            <ProtectedRoute isRequireAuth={true}>
+                <Suspense>
+                    <HomePage />
+                </Suspense>
+            </ProtectedRoute>
+        ),
+        errorElement: <NotFound />,
     },
-])
+    {
+        path: 'sign-up',
+        element: (
+            <ProtectedRoute isRequireNoAuth={true}>
+                <Suspense>
+                    <SignInPage />
+                </Suspense>
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: 'sign-in',
+        element: (
+            <ProtectedRoute isRequireNoAuth={true}>
+                <Suspense>
+                    <SignUpPage />
+                </Suspense>
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '*',
+        element: <NotFound />,
+    },
+]);
