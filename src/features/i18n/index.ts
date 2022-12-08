c1ccd30fcbf9424b2ebab67df77c4abd1c7c1572
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-08 22:55:12 +0800

    refactor: no need debug

--- src/features/i18n/index.ts
+++ src/features/i18n/index.ts
@@ -2,7 +2,6 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import { isDev } from '@/features/utils'
 
 export default i18n
     .use(Backend)
@@ -13,7 +12,6 @@ export default i18n
             order: ['querystring', 'navigator'],
             caches: [], // no need caches
         },
-        debug: isDev,
         interpolation: {
             escapeValue: false,
         },
