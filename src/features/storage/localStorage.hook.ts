1a82fed937e3b4a7a9b2f922f02978363db104c8
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-08 23:00:41 +0800

    feat: create useLocalStorage hook

---
+++ src/features/storage/localStorage.hook.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect, useCallback, useRef } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
+
+export function useLocalStorage(
+    key: string
+): [string | null, Dispatch<SetStateAction<string | null>>] {
+    const defaultValue = window.localStorage.getItem(key);
+    const [state, setState] = useState<null | string>(defaultValue);
+    const isFirstEffect = useRef(true);
+
+    /** cache storage event callback */
+    const handleStorageEvent = useCallback(
+        (event: StorageEvent) => {
+            if (event.key !== key) return;
+            setState(event.newValue);
+        },
+        [key]
+    );
+
+    /** handle storage event */
+    useEffect(() => {
+        window.addEventListener('storage', handleStorageEvent);
+        return () => {
+            window.removeEventListener('storage', handleStorageEvent);
+        };
+    }, [handleStorageEvent]);
+
+    /** if state update,localstorage also update. */
+    useEffect(() => {
+        // no need update localstorage when first rendering.
+        if (isFirstEffect.current) {
+            isFirstEffect.current = false;
+            return;
+        }
+        window.localStorage.setItem(key, state || '');
+    }, [key, state]);
+
+    return [state, setState];
+}
