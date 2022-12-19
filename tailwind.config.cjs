83844ff2a675312b0270306eb36ad5abe1f26622
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-19 22:47:44 +0800

    feat: add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    300: '#FFC1AB',
+                    400: '#FFA889',
+                    500: '#FF8356',
+                    600: '#FF663C',
+                    700: '#FF532B',
+                },
+                red: {
+                    700: '#E52A2A',
+                },
+                green: {
+                    700: '#21AB0B',
+                },
+                black: {
+                    50: '#F5F5F5',
+                    100: '#E4E4E4',
+                    200: '#BCBCBC',
+                    400: '#8F8F8F',
+                    600: '#626262',
+                    800: '#404040',
+                    900: '#1E1E1E',
+                },
+            },
+        },
     },
     plugins: [],
 };
