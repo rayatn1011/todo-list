ca1e447262df7fd6e05ceb9230b826abeccc4ac6
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-24 23:43:05 +0800

    feat: update button style

--- src/features/ui/base/button.component.tsx
+++ src/features/ui/base/button.component.tsx
@@ -20,16 +20,16 @@ export function Button({
   isFullWidth = false,
 }: IButton) {
   const defaultClass =
-    'flex items-center gap-x-1 justify-center rounded border px-6 py-3 text-xl disabled:border-black-200 disabled:bg-black-200 transition';
+    'flex items-center gap-x-1 justify-center rounded border px-6 py-3 text-xl outline-none disabled:border-black-200 disabled:bg-black-200 transition';
   const customClassConfig = {
     filled: {
       primary:
-        'border-primary-500 bg-primary-500 text-white hover:bg-primary-600 hover:border-primary-600 active:bg-primary-600 active:border-primary-600 active:shadow-[0_0_0_0.25rem] active:shadow-primary-600/30',
+        'border-primary-500 bg-primary-500 text-white hover:bg-primary-450 hover:border-primary-450 hover:shadow-md active:bg-primary-600 active:border-primary-600 focus:shadow-[0_0_0_0.25rem] focus:shadow-primary-600/30',
       default: '',
     },
     outlined: {
       primary:
-        'border-primary-500 bg-white text-primary-500 hover:text-primary-600 hover:border-primary-600 active:border-primary-600 active:text-primary-700 active:shadow-[0_0_0_0.25rem] active:shadow-primary-600/30',
+        'border-primary-500 bg-white text-primary-500 hover:text-primary-450 hover:border-primary-450 hover:shadow-md active:border-primary-600 active:text-primary-600 focus:shadow-[0_0_0_0.25rem] focus:shadow-primary-600/30',
       default: '',
     },
     text: {
